4e75aa48b5b1f80b96a85ce710e7f582cfd9b28d
cmichel <cmichel@nelogica.com.br>
2024-12-26 09:11:31 -0300

    pass exceptions

--- main.py
+++ main.py
@@ -13,29 +13,31 @@ def verify_log(file_path):
         lines = f.readlines()
 
     for i, line in enumerate(lines, start=1):
-        
-        # armazena nome da unit e da classe (nao mudam)
-        find_unit = re.search(rf'unit.*;', line)
-        if find_unit:
-            unit_name = line.split(' ')[1]
-            unit_name = re.split(rf';', unit_name)[0]
-            class_name = 'T' + unit_name[:-1]
+        try:
+            # armazena nome da unit e da classe (nao mudam)
+            find_unit = re.search(rf'unit.*;', line)
+            if find_unit:
+                unit_name = line.split(' ')[1]
+                unit_name = re.split(rf';', unit_name)[0]
+                class_name = 'T' + unit_name[:-1]
 
-        # armazena nome do metodo (muda a cada metodo)
-        find_method = re.search(rf'(procedure|function|constructor|destructor).*{class_name}\.', line)
-        if find_method:
-            method_name = line.split('.')[1]
-            method_name = re.split(rf';|\(|:', method_name)[0]
+            # armazena nome do metodo (muda a cada metodo)
+            find_method = re.search(rf'(procedure|function|constructor|destructor).*{class_name}\.', line)
+            if find_method:
+                method_name = line.split('.')[1]
+                method_name = re.split(rf';|\(|:', method_name)[0]
 
-        # armazena log de LogError, LogException ou m_EventWriter
-        find_log = re.search(rf'(LogError\(|LogException\(|EventWriter.WriteMsg\(\')', line)
-        if find_log:
-            log_name = line.split('\'')[1]
-            log_name = re.split(rf':| ', log_name, 1)[0]
+            # armazena log de LogError, LogException ou m_EventWriter
+            find_log = re.search(rf'(LogError\(|LogException\(|EventWriter.WriteMsg\(\')', line)
+            if find_log:
+                log_name = line.split('\'')[1]
+                log_name = re.split(rf':| ', log_name, 1)[0]
 
-            expected_log = f'{class_name}.{method_name}'
-            if log_name.strip() != expected_log.strip():
-                line_errors.append((i, log_name, expected_log))
+                expected_log = f'{class_name}.{method_name}'
+                if log_name.strip() != expected_log.strip():
+                    line_errors.append((i, log_name, expected_log))
+        except:
+            pass
 
     return line_errors
 
